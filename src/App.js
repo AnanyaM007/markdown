67f8f1b19fe2c6d6d4d481a89e705efd0063adf3
Ananya <99496164+AnanyaM007@users.noreply.github.com>
2024-08-23 23:21:56 +0530

    [feat] markdown added

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Markdown from './components/markdown/markdown';
+
+function App() {
+  return (
+    <div className="App">
+      <Markdown />
+    </div>
+  );
+}
+
+export default App;
